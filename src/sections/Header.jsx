b42ee63751a34637fc9c2032edde11eb23766903
Ayushman Sen <ayushmansen500@gmail.com>
2026-02-17 14:39:32 +0530

    Update Header.jsx

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -36,18 +36,25 @@ const Header = () => {
     <header
       className={clsx(
         "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
-        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
       )}
     >
       <div className="container flex h-14 items-center max-lg:px-5">
+        {/* Mobile Logo */}
         <a className="lg:hidden flex-1 cursor-pointer z-2">
-          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+          <img
+            src="/images/xora.svg"
+            width={115}
+            height={55}
+            alt="CloudDevOpsHub Logo"
+          />
         </a>
 
+        {/* Navigation */}
         <div
           className={clsx(
             "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
-            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
           )}
         >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
@@ -59,21 +66,20 @@ const Header = () => {
                   <NavLink title="pricing" />
                 </li>
 
+                {/* Center Logo */}
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
                     offset={-250}
                     spy
                     smooth
-                    className={clsx(
-                      "max-lg:hidden transition-transform duration-500 cursor-pointer",
-                    )}
+                    className="max-lg:hidden transition-transform duration-500 cursor-pointer"
                   >
                     <img
                       src="/images/xora.svg"
                       width={160}
                       height={55}
-                      alt="logo"
+                      alt="CloudDevOpsHub"
                     />
                   </LinkScroll>
                 </li>
@@ -86,32 +92,35 @@ const Header = () => {
               </ul>
             </nav>
 
+            {/* Mobile Background Graphics */}
             <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
               <img
                 src="/images/bg-outlines.svg"
                 width={960}
                 height={380}
-                alt="outline"
+                alt="background outline"
                 className="relative z-2"
               />
               <img
                 src="/images/bg-outlines-fill.png"
                 width={960}
                 height={380}
-                alt="outline"
+                alt="background fill"
                 className="absolute inset-0 mix-blend-soft-light opacity-5"
               />
             </div>
           </div>
         </div>
 
+        {/* Mobile Menu Button */}
         <button
           className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
           onClick={() => setIsOpen((prevState) => !prevState)}
+          aria-label="Toggle navigation"
         >
           <img
             src={`/images/${isOpen ? "close" : "magic"}.svg`}
-            alt="magic"
+            alt="menu toggle"
             className="size-1/2 object-contain"
           />
         </button>
